f29415c49ed355e4a82c55b781cc4c6557327c95
Santiago Maldonado Rojas <santiagomaldonadorojas64@gmail.com>
2024-10-01 05:57:48 -0500

    Se arreglo un error que estaba en null hero

--- src/null_models/null.hero.ts
+++ src/null_models/null.hero.ts
@@ -2,7 +2,7 @@ import { Hero } from "../models/hero.model.js";
 
 export class NullHero extends Hero {
     constructor() {
-        super("idUser", "rogue", "fire", [], [], "blue");
+        super("idUser", "rogue", "fire", [], [], "blue",0);//Se agrego el 0 de los creditos
     }
 
     isNull(): boolean { return true; }
